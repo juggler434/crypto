2d444e9d4640c30f506a18bbb3ddaa84f2cbf44e
Bill Gallagher <bill.gallagher@uber.com>
2019-12-05 13:28:27 -0800

    implements set 1 challenge 3 business logic

--- set1/challenge3.go
+++ set1/challenge3.go
@@ -1,14 +1,58 @@
 package cryptopals
 
-//SingleXorCiphter takes an encoded message and decodes it into a human readable string
-func SingleXorCipher(codedMessage []byte) []byte, error{
-	//Input: Byte Slice (Hex encoded, xor'd)
-	//Output: Byte Slice (Human readable)
+//SingleXorCipher takes an encoded message and decodes it into a human readable string
+func SingleXorCipher(codedMessage []byte) ([]byte, error) {
+	b, err := decodeHexBytes(codedMessage)
+	if err != nil {
+		return nil, err
+	}
+	var answer []byte
+	var score int
+	for i := 0; i < 256; i++ {
+		r := make([]byte, len(b))
+		var s int
+		for j := 0; j < len(b); j++ {
+			c := b[j] ^ byte(i)
+			s += getCharWeight(c)
+			r[j] = c
+		}
+		if s > score {
+			answer = r
+			score = s
+		}
 
-	// Steps:
-	// 1. Decode slice from hex
-	// 2. XOR the decoded slice against a character
-	// 3. Loop throught the result, scoring based on character frequency
-	// 4. If score is higher than previous best score, save result
-	// 5. Return
+		s = 0
+	}
+	return answer, nil
+}
+
+func getCharWeight(char byte) int {
+	wm := map[byte]int{
+		byte('U'): 1,
+		byte('u'): 1,
+		byte('L'): 2,
+		byte('l'): 2,
+		byte('D'): 3,
+		byte('d'): 3,
+		byte('R'): 4,
+		byte('r'): 4,
+		byte('H'): 5,
+		byte('h'): 5,
+		byte('S'): 6,
+		byte('s'): 6,
+		byte(' '): 7,
+		byte('N'): 8,
+		byte('n'): 8,
+		byte('I'): 9,
+		byte('i'): 9,
+		byte('O'): 10,
+		byte('o'): 10,
+		byte('A'): 11,
+		byte('a'): 11,
+		byte('T'): 12,
+		byte('t'): 12,
+		byte('E'): 13,
+		byte('e'): 13,
+	}
+	return wm[char]
 }
