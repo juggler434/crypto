b6b8248d541c9fc73a3aa5dcfa675ad17468b679
Bill Gallagher <bill.gallagher@uber.com>
2019-12-11 09:51:55 -0800

    adds business logic for set 1 challenge 5

--- set1/challenge5.go
+++ set1/challenge5.go
@@ -1,6 +1,18 @@
 package cryptopals
 
+import (
+	"encoding/hex"
+)
+
+//EncryptWithRepeatingXor XOR encrypts a given string with a given key.  Returns a hex encoded string.
 func EncryptWithRepeatingXor(input, key []byte) ([]byte, error) {
-	var ret []byte
+	eb := make([]byte, len(input))
+
+	for i := 0; i < len(input); i++ {
+		eb[i] = input[i] ^ key[i%len(key)]
+	}
+
+	ret := make([]byte, hex.EncodedLen(len(eb)))
+	hex.Encode(ret, eb)
 	return ret, nil
 }
--- set1/challenge5_test.go
+++ set1/challenge5_test.go
@@ -2,14 +2,15 @@ package cryptopals
 
 import "testing"
 
+const RepeatingXorTestString = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+
+const RepeatingXorTestResult = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
 func TestEncryptWithRepeatingXor(t *testing.T) {
 	t.Run("with valid input", func(t *testing.T) {
-		input := []byte(`Burning 'em, if you ain't quick and nimble
-		I go crazy when I hear a cymbal`)
+		input := []byte(RepeatingXorTestString)
 		key := []byte("ICE")
-		expected :=
-			`0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
-a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f`
+		expected := RepeatingXorTestResult
 		res, err := EncryptWithRepeatingXor(input, key)
 		if err != nil {
 			t.Errorf("Expected: error to be nil, got: %s", err)
