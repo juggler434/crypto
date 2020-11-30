dab797c123b9d8bc0746aa9e01d4f3f232d721bb
juggler434 <wgallagher86@gmail.com>
2020-11-29 20:30:36 -0800

    fix bug in test

--- encoding/hex/to_base_64_test.go
+++ encoding/hex/to_base_64_test.go
@@ -1,32 +1,48 @@
 package hex
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestHexToBase64(t *testing.T) {
-	t.Run("with valid input", func(t *testing.T) {
-		//These strings are pulled from the crypto pals challenge page
-		hb := []byte("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
-		expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-		ret, err := ToBase64(hb)
-		if err != nil {
-			t.Errorf("expected error to be nil, got: %s", err)
-		}
-		if string(ret) != expected {
-			t.Errorf("expected: %s, got: %s", expected, ret)
-		}
-	})
 
-	t.Run("with invalid input", func(t *testing.T) {
-		hb := []byte("This is not a valid hex string")
-		ret, err := ToBase64(hb)
-		if ret != nil {
-			t.Errorf("expected: nil, got: %s", ret)
-		}
+	tests := []struct {
+		name           string
+		input          []byte
+		expectedOutput []byte
+		shouldError    bool
+	}{
+		{
+			name:           "with valid input",
+			input:          []byte("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"),
+			expectedOutput: []byte("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"),
+			shouldError:    false,
+		},
+		{
+			name:           "with non-valid hex string",
+			input:          []byte("This is not a valid hex string"),
+			expectedOutput: nil,
+			shouldError:    true,
+		},
+	}
 
-		if err == nil {
-			t.Error("expected: error to not be nil, got: nil")
-		}
-	})
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ret, err := ToBase64(test.input)
+			if test.shouldError {
+				if err == nil {
+					t.Error("expected: error, got: nil")
+				}
+			} else {
+				if err != nil {
+					t.Errorf("expected: nil error, got: %s", err)
+				}
+			}
+
+			if !bytes.Equal(ret, test.expectedOutput) {
+				t.Errorf("expected: %s, got: %s", test.expectedOutput, ret)
+			}
+		})
+	}
 }
