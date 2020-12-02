aaf851273ef8bcba063fd2473995df5986f8672d
juggler434 <wgallagher86@gmail.com>
2020-12-02 14:04:15 -0800

    implements business logic for challenge 9

---
+++ padding/pkcs7_test.go
@@ -0,0 +1,41 @@
+package padding
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestPKCS7(t *testing.T) {
+	tests := []struct {
+		name           string
+		input          []byte
+		blockSize      int
+		expectedOutput []byte
+	}{
+		{
+			name:           "block size greater than input",
+			input:          []byte("YELLOW SUBMARINE"),
+			blockSize:      20,
+			expectedOutput: []byte("YELLOW SUBMARINE\x04\x04\x04\x04"),
+		}, {
+			name:           "block size smaller than input",
+			input:          []byte("YELLOW SUBMARINE"),
+			blockSize:      6,
+			expectedOutput: []byte("YELLOW SUBMARINE\x02\x02"),
+		}, {
+			name:           "input multiple of block size",
+			input:          []byte("YELLOW SUBMARINE"),
+			blockSize:      4,
+			expectedOutput: []byte("YELLOW SUBMARINE"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res := PKCS7(test.input, test.blockSize)
+			if !bytes.Equal(res, test.expectedOutput) {
+				t.Errorf("expected: %+q, got: %+q", test.expectedOutput, res)
+			}
+		})
+	}
+}
