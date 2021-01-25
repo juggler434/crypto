136d3950893719a9e9d3a4a1e2933adfd05b8b42
juggler434 <wgallagher86@gmail.com>
2021-01-24 18:00:57 -0800

    refactors oracle tests

--- aes128/oracle/cbc_test.go
+++ aes128/oracle/cbc_test.go
@@ -10,9 +10,10 @@ import (
 )
 
 const cbcTestFile = "./test_files/cbc_oracle_test.txt"
+const cbcBadTestFile = "./test_files/cbc_bad_data.txt"
 
-func readTestFile(t *testing.T) [][]byte {
-	f, err := ioutil.ReadFile(cbcTestFile)
+func readTestFile(t *testing.T, inputFile string) [][]byte {
+	f, err := ioutil.ReadFile(inputFile)
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
@@ -22,7 +23,7 @@ func readTestFile(t *testing.T) [][]byte {
 }
 
 func TestNewCBCOracle(t *testing.T) {
-	l := readTestFile(t)
+	l := readTestFile(t, cbcTestFile)
 
 	r, err := NewCBCOracle(l)
 	if err != nil {
@@ -39,36 +40,85 @@ func TestNewCBCOracle(t *testing.T) {
 }
 
 func TestCBCOracle_Encrypt(t *testing.T) {
-	l := readTestFile(t)
-	r, err := NewCBCOracle(l)
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
+	tests := []struct {
+		name      string
+		inputFile string
+		shouldErr bool
+	}{
+		{
+			name:      "base case",
+			inputFile: cbcTestFile,
+			shouldErr: false,
+		}, {
+			name:      "malformed base64",
+			inputFile: cbcBadTestFile,
+			shouldErr: true,
+		},
 	}
 
-	o := r.(*CBCOracle)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			l := readTestFile(t, test.inputFile)
+			r, err := NewCBCOracle(l)
+			if err != nil {
+				t.Errorf("unexpected error: %s", err)
+			}
 
-	iv := make([]byte, 16)
-	_, err = rand.Read(iv)
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
+			o := r.(*CBCOracle) // Do this so we can get the key to decrypt test data
+
+			iv := make([]byte, 16)
+			_, err = rand.Read(iv)
+			if err != nil {
+				t.Errorf("unexpected error: %s", err)
+			}
+
+			res, err := r.Encrypt(iv)
+
+			if test.shouldErr {
+				if err == nil {
+					t.Error("expected: err, got: nil")
+				}
+				return
+			} else {
+				if err != nil {
+					t.Errorf("expected: nil error, got: %s", err)
+				}
+			}
+
+			v, err := base64.Decode(res)
+			if err != nil {
+				t.Errorf("unexptected error: %s", err)
+			}
+
+			riv := v[:16]
+			if !bytes.Equal(iv, riv) {
+				t.Errorf("expected first 16 bytes to equal: %s, got %s", iv, riv)
+			}
+
+			e := v[17:]
+
+			v, err = cbc.Decrypt(e, o.Key, iv)
+			if err != nil {
+				t.Errorf("unexpected error: %s", err)
+			}
+
+			if !comparePlainText(v) {
+				t.Errorf("expected decrypted value to match, one of tests values, got: %s", v)
+			}
+		})
+	}
+}
+
+func comparePlainText(plaintext []byte) bool {
+	testInputs := [][]byte{[]byte("This line is really long and is more than one block"),
+		[]byte("Tacos are cool"),
+		[]byte("Third input"),
 	}
 
-	res, err := r.Encrypt(iv)
-	if err != nil {
-		t.Errorf("unexptected error: %s", err)
+	for _, input := range testInputs {
+		if bytes.Equal(input, plaintext) {
+			return true
+		}
 	}
-
-	v, _ := base64.Decode(res)
-
-	riv := v[:16]
-	if !bytes.Equal(iv, riv) {
-		t.Errorf("expected first 16 bytes to equal: %s, got %s", iv, riv)
-	}
-
-	e := v[17:]
-
-	_, err = cbc.Decrypt(e, o.Key, iv)
-	if err != nil {
-		t.Errorf("expected nil error, got: %s", err)
-	}
+	return false
 }
